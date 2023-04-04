e8df4e8df1bdc62c9bdb57f03179eaaadbea66b2
Deniz Ök <deniz.ok2019@gmail.com>
2023-04-04 19:11:59 +0300

    added Product List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
+import ProductPage from './ProductPage';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route index element={<Layout/>}/>
-          
+          <Route path='/productlist' element={<ProductPage/>}/>
         <Route/>
       </Routes>
     </div>
